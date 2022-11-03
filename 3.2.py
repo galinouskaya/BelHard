67d350b6077bfb89539475e8237cf7966dc1509e
Hanna Halinouskaya <galinouskaya@gmail.com>
2022-11-03 16:47:21 +0300

    Task 2

---
+++ 3.2.py
@@ -0,0 +1,5 @@
+first_number = int(input('enter first number: '))
+second_number = int(input('enter second number: '))
+third_number = int(input('enter third number: '))
+avg = (first_number+second_number+third_number)/3
+print('average of numbers is: ', round(avg, 3))
