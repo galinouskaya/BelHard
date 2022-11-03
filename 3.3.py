6eb15ce65ec6d22d3d98381dd1539117a89f7488
Hanna Halinouskaya <galinouskaya@gmail.com>
2022-11-03 19:05:26 +0300

    three ways

--- 3.3.py
+++ 3.3.py
@@ -1,10 +1,22 @@
 # First way
 
-name = input('enter your name ')
-age = input('enter your age ')
-city = input('enter your city ')
+name = input('enter your name: ')
+age = input('enter your age: ')
+city = input('enter your city: ')
 greetings = 'Hello {name}, {age} age, from {city}'.format(name=name.title(), age=age, city=city.title())
 print(greetings)
 
+# Second way
 
+name = input('enter your name: ')
+age = input('enter your age: ')
+city = input('enter your city: ')
+greetings = f'Hello {name}, {age} age, from {city}'
+print(greetings)
 
+# Third way
+
+name = input('enter your name: ')
+age = input('enter your age: ')
+city = input('enter your city: ')
+print('Hello ' + name.title() + (', ') + age + (' age,') + ' from ' + city.title())
